25a4d5809f7111eb272ada8a3643b449adf18b0f
Alex Truslow <atruslow@mercury.com>
2024-03-27 16:16:52 -0400

    Making the server async

--- app/main.py
+++ app/main.py
@@ -7,24 +7,6 @@ logger = logging.getLogger(__name__)
 
 REDIS_PORT = 6379
 
-# def main():
-#     # You can use print statements as follows for debugging, they'll be visible when running tests.
-#     print("Logs from your program will appear here!")
-
-#     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-#     conn, addr = server_socket.accept() # wait for client
-
-#     with conn:
-#         while True:
-#             data = conn.recv(1024)
-#             msg = data.decode()
-#             if not msg:
-#                 continue
-#             logger.info(f"Received {msg}")
-#             conn.sendall("+PONG\r\n".encode())
-#             logger.info("Sent +PONG")
-
-
 import asyncio
 
 async def handle_client(reader, writer):
