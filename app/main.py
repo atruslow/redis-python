ffd768c5de8d958c471c6b95fda678ef0b8f89f8
Alex Truslow <atruslow@mercury.com>
2024-03-27 15:24:14 -0400

    Passing 2nd Stage

--- app/main.py
+++ app/main.py
@@ -6,7 +6,17 @@ def main():
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    conn, addr = server_socket.accept() # wait for client
+
+    with conn:
+        while True:
+            data = conn.recv(1024)
+            msg = data.decode()
+            if not msg:
+                break
+            print(f"Received {msg}")
+            conn.sendall("+PONG\r\n".encode())
+            print("Sent +PONG")
 
 
 if __name__ == "__main__":
