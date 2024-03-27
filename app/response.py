396f5d6ed6da0efb171827aa0400dca817c85e8a
Alex Truslow <atruslow@mercury.com>
2024-03-27 17:50:09 -0400

    Parsing commands

--- app/response.py
+++ app/response.py
@@ -1,5 +1,18 @@
 import asyncio
+from dataclasses import dataclass
+from enum import Enum
 from typing import List
+
+
+class Command(Enum):
+    Ping = 1
+    Echo = 2
+
+
+@dataclass
+class ParsedCommand:
+    command: Command
+    args: List[str]
 
 
 async def async_parse(msg: str) -> List[str]:
@@ -17,3 +30,15 @@ def parse(msg: str) -> List[str]:
     ]
 
     return parsed_list
+
+
+def parse_command(msg: List[str]) -> ParsedCommand:
+    cmd, *rest = msg
+
+    match cmd.lower():
+        case "ping":
+            return ParsedCommand(command=Command.Ping, args=rest)
+        case "echo":
+            return ParsedCommand(command=Command.Echo, args=rest)
+        case _:
+            raise RuntimeError("Bad Command")
