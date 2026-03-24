c34044e80773b5f79250154631cfc4d0d5fb0d86
Alexander Truslow <alextruslow@gmail.com>
2026-03-23 21:53:53 -0400

    fixing

--- app/replica/handshake.py
+++ app/replica/handshake.py
@@ -16,12 +16,19 @@ async def handshake(master_host: str, master_port: str, server_port: str) -> Non
 
     writer.write(resp_parser.encode([b"PING"]))
     await writer.drain()
+    resp = await reader.read(1024)
+    logger.info(f"Master PING resp: {resp.decode()}")
 
     writer.write(resp_parser.encode([b"REPLCONF", b"listening-port", str(server_port).encode()]))
     await writer.drain()
+    resp = await reader.read(1024)
+    logger.info(f"Master REPLCONF listening-port resp: {resp.decode()}")
 
     writer.write(resp_parser.encode([b"REPLCONF", b"capa", b"psync2"]))
     await writer.drain()
+    resp = await reader.read(1024)
+    logger.info(f"Master REPLCONF capa resp: {resp.decode()}")
+
     logger.info("Completed handshake with master")
 
     writer.close()
