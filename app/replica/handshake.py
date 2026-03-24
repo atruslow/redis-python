442ceb8e60a02a58a1dfc5cdb31185f041fc55fd
Alexander Truslow <alextruslow@gmail.com>
2026-03-23 22:01:19 -0400

    fixing

--- app/replica/handshake.py
+++ app/replica/handshake.py
@@ -10,26 +10,24 @@ async def handshake(master_host: str, master_port: str, server_port: str) -> Non
     """
     Completes a handshake with the master if we are a slave
     """
-
     logger.info(f"Connecting to master {master_host}:{master_port}")
     reader, writer = await asyncio.open_connection(master_host, master_port)
 
-    writer.write(resp_parser.encode([b"PING"]))
-    await writer.drain()
-    resp = await reader.read(1024)
-    logger.info(f"Master PING resp: {resp.decode()}")
-
-    writer.write(resp_parser.encode([b"REPLCONF", b"listening-port", str(server_port).encode()]))
-    await writer.drain()
-    resp = await reader.read(1024)
-    logger.info(f"Master REPLCONF listening-port resp: {resp.decode()}")
-
-    writer.write(resp_parser.encode([b"REPLCONF", b"capa", b"psync2"]))
-    await writer.drain()
-    resp = await reader.read(1024)
-    logger.info(f"Master REPLCONF capa resp: {resp.decode()}")
+    await _send(reader, writer, [b"PING"])
+    await _send(reader, writer, [b"REPLCONF", b"listening-port", str(server_port).encode()])
+    await _send(reader, writer, [b"REPLCONF", b"capa", b"psync2"])
 
     logger.info("Completed handshake with master")
-
     writer.close()
     await writer.wait_closed()
+
+
+async def _send(
+    reader: asyncio.StreamReader,
+    writer: asyncio.StreamWriter,
+    args: list,
+) -> None:
+    writer.write(resp_parser.encode(args))
+    await writer.drain()
+    resp = await reader.read(1024)
+    logger.info(f"Master response: {resp.decode()}")
