1eeada0eb9758673b1634c5d7d2e13142af02466
Alexander Truslow <alextruslow@gmail.com>
2026-03-26 18:41:20 -0400

    Next stage

--- app/command/wait.py
+++ app/command/wait.py
@@ -11,8 +11,8 @@ def handle_wait(args: List[str]) -> ParsedCommand:
     num_replicas, timeout = args
 
     replica_count = replication.num_replicas()
-    
 
-    time.sleep(int(timeout))
+
+    #time.sleep(int(timeout))
 
     return ParsedCommand(command=Command.Wait, args=args, response=replica_count)
