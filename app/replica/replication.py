38e99649554e2a7b1c8f6dc254638f6e6db64432
Alexander Truslow <alextruslow@gmail.com>
2026-03-26 09:29:28 -0400

    first stab at replication

--- app/replica/replication.py
+++ app/replica/replication.py
@@ -14,7 +14,7 @@ async def replication(reader: StreamReader, writer: StreamWriter) -> None:
     Replicates data from a master without responding
     """
     
-    while resp := await reader.read():
+    while resp := await reader.read(1024):
 
         command = resp.decode("utf-8")
         value = parser.parse_list(command)
